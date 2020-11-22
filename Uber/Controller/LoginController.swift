b09e5106f7a2e93afa14ad034a1e29cfd640037c
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-22 19:01:32 -0300

    setting custom root view controller

--- Uber/Controller/LoginController.swift
+++ Uber/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  Uber
 //
 //  Created by Ezequiel Parada Beltran on 22/11/2020.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
+    
 }
 
