d47341580bb1901d97b13a88d28ca2c5cb6dba50
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-23 20:55:35 -0300

    finishing signUpVC

--- Uber/Controller/SignUpController.swift
+++ Uber/Controller/SignUpController.swift
@@ -9,17 +9,136 @@ import UIKit
 
 class SignUpController: UIViewController {
     //MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private lazy var fullNameContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_person_outline_white_2x"), textField: fullNameTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private lazy var accountTypeContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_account_box_white_2x"), segmentedControl: accountTypeSegmentedControl)
+        view.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Email",
+                                       isSecureTextEntry: false)
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password",
+                                       isSecureTextEntry: true)
+    }()
+    
+    private let fullNameTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Fullname",
+                                       isSecureTextEntry: true)
+    }()
+    
+    private let accountTypeSegmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Rider", "Driver"])
+        sc.backgroundColor = .backgroundColor
+//        sc.selectedSegmentTintColor = UIColor(white: 1, alpha: 0.87)
+        
+        
+        sc.layer.borderColor = UIColor(white: 1, alpha: 0.87).cgColor
+        sc.selectedSegmentTintColor = UIColor(white: 1, alpha: 0.87)
+        sc.layer.borderWidth = 1
+        
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        sc.setTitleTextAttributes(titleTextAttributes, for:.normal)
+        
+        let titleTextAttributes1 = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        sc.setTitleTextAttributes(titleTextAttributes1, for:.selected)
+        
+        
+        sc.selectedSegmentIndex = 0
+        return sc
+    }()
+    
+    private let signUpButton: AuthButton = {
+        let button = AuthButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        return button
+    }()
+    
+    let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Already have an account?  ", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        attributedTitle.append(NSAttributedString(string: "Log In", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.mainBLueTint]))
+        
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
 
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        configureUI()
     }
 
 
     //MARK: - Selectors
+    @objc func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: - Helper functions
+    
+    func configureUI() {
+        
+        
+        
+        view.backgroundColor = .backgroundColor
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+        
+    let stack = UIStackView(arrangedSubviews: [emailContainerView,fullNameContainerView, passwordContainerView, accountTypeContainerView, signUpButton, alreadyHaveAccountButton])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 24
+        
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
+        
+        view.addSubview(alreadyHaveAccountButton)
+        alreadyHaveAccountButton.centerX(inView: view)
+        alreadyHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
+        
+    }
+    
+    
 
 }
 
